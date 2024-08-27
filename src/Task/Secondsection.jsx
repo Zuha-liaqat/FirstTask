883c72d4334ac94148197d147c3b44fef6a8d81d
ZuhaLiaqat <zuhaliaqat996@gmail.com>
2024-08-27 12:57:52 +0500

    zuha

--- src/Task/Secondsection.jsx
+++ src/Task/Secondsection.jsx
@@ -13,56 +13,56 @@ function Secondsection() {
         </Grid>
 
         <Grid item xs={12} sm={6}>
-          <Box className="textbox">
+          <Box className="textbox" marginLeft={3}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={8}>
-                <Typography variant="h6">Jack Fruit Tacos</Typography>
-                <Typography variant="body2">40z</Typography>
-                <Typography variant="body2">
+                <Typography variant="h6" color={'#2F75E6'}>Jack Fruit Tacos</Typography>
+                <Typography variant="body2" color={'#2F75E6'}>40z&nbsp; 80z</Typography>
+                <Typography variant="body2"  color={'#2F75E6'}>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit dolor consequat netus tristique.
                 </Typography>
               </Grid>
               
               <Grid item xs={12} sm={4}>
-                <Typography variant="h6" align="left">
+                <Typography  color={'#2F75E6'} variant="h6" align="left">
                   $15.00
                 </Typography>
               </Grid>
             </Grid>
-            <Divider sx={{ my: 2, borderColor: 'grey', borderWidth: 0.5, width: "90%" }}/>
+            <Divider sx={{ my: 2, borderColor: 'grey', borderWidth: 0.5, width: "80%" }}/>
           </Box>
           
-          <Box className="textbox">
+          <Box className="textbox" marginLeft={3}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={8}>
-                <Typography variant="h6">Baja Cali Tacos</Typography>
-                <Typography variant="body2">40z</Typography>
-                <Typography variant="body2">
+                <Typography variant="h6"  color={'#2F75E6'}>Baja Cali Tacos</Typography>
+                <Typography variant="body2"  color={'#2F75E6'}>40z&nbsp; 80z</Typography>
+                <Typography variant="body2"  color={'#2F75E6'}>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit dolor consequat netus tristique.
                 </Typography>
               </Grid>
               
               <Grid item xs={12} sm={4}>
-                <Typography variant="h6" align="left">
+                <Typography  color={'#2F75E6'}variant="h6" align="left">
                   $25.00
                 </Typography>
               </Grid>
             </Grid>
-            <Divider sx={{ my: 2, borderColor: 'grey', borderWidth: 0.5, width: "90%" }}/>
+            <Divider sx={{ my: 2, borderColor: 'grey', borderWidth: 0.5, width: "80%" }}/>
           </Box>
 
-          <Box className="textbox">
+          <Box className="textbox" marginLeft={3}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={8}>
-                <Typography variant="h6">Impossible Tacos</Typography>
-                <Typography variant="body2">40z</Typography>
-                <Typography variant="body2">
+              <Grid item xs={12} sm={8} >
+                <Typography   color={'#2F75E6'} variant="h6">Impossible Tacos</Typography>
+                <Typography  color={'#2F75E6'} variant="body2">40z&nbsp; 80z</Typography>
+                <Typography  color={'#2F75E6'} variant="body2">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit dolor consequat netus tristique.
                 </Typography>
               </Grid>
               
               <Grid item xs={12} sm={4}>
-                <Typography variant="h6" align="left">
+                <Typography  color={'#2F75E6'} variant="h6" align="left">
                   $35.00
                 </Typography>
               </Grid>
